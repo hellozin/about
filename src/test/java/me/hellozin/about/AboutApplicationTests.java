6284b977cd91dfa4c384e7961418e2072ce37331
hellozin <paul26375@gmail.com>
2020-11-29 01:41:08 +0900

    Create Repo

---
+++ src/test/java/me/hellozin/about/AboutApplicationTests.java
@@ -0,0 +1,13 @@
+package me.hellozin.about;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AboutApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
